ec807342da32e7c7d64e5a372125b676ec7fd935
Telence <thaylan.gladiuz@gmail.com>
2016-06-20 20:07:54 -0300

    Update 367_misc.sql

--- SQL/Updates/World/367_misc.sql
+++ SQL/Updates/World/367_misc.sql
@@ -5,9 +5,9 @@ UPDATE `spell_bonus_data` SET `ap_bonus`='0.6' WHERE `entry`='114014';
 UPDATE `spell_bonus_data` SET `ap_bonus`='2.55' WHERE `entry`='5308';
 
 -- Daybreak for Holy Shock damage spell
-INSERT INTO `spell_script_names` VALUES (25912, 'spell_pal_holy_shock_heal');
+REPLACE INTO `spell_script_names` VALUES (25912, 'spell_pal_holy_shock_heal');
 
 -- fix removing of Holy Spark (Item : S12 2P bonus - Heal)
-INSERT INTO `spell_script_names` VALUES (2060, 'spell_pri_holy_spark');  -- Greater Heal
-INSERT INTO `spell_script_names` VALUES (88684, 'spell_pri_holy_spark'); -- Holy Word: Serenity
-INSERT INTO `spell_script_names` VALUES (2061, 'spell_pri_holy_spark');  -- Flash heal
+REPLACE INTO `spell_script_names` VALUES (2060, 'spell_pri_holy_spark');  -- Greater Heal
+REPLACE INTO `spell_script_names` VALUES (88684, 'spell_pri_holy_spark'); -- Holy Word: Serenity
+REPLACE INTO `spell_script_names` VALUES (2061, 'spell_pri_holy_spark');  -- Flash heal
